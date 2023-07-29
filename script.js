9f323782212e2827eb76d7ed8291f7929898fb75
Funtic-from-Belarus <maksim.kuzyuk@gmail.com>
2023-07-29 17:18:24 +0300

    Connect frontend to backend
    Add new page with reviews. An user can write own rewiew and it added to the MongoDB. Also in future the user can delete and edit reviews

--- script.js
+++ script.js
@@ -1,50 +1,27 @@
-const APILINK = "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=3832f848d12ad047efdd0c9e4b297320&page=1";
-const IMG_PATH = "https://image.tmdb.org/t/p/w1280";
-const SEARCHAPI = "https://api.themoviedb.org/3/search/movie?&api_key=3832f848d12ad047efdd0c9e4b297320&query=";
+import app from "./server.js";
+import mongodb from "mongodb";
+import ReviewsDAO from "./dao/reviewsDAO.js";
 
+const MongoClient = mongodb.MongoClient;
+const mongo_username = process.env['MONGO_USERNAME'];
+const mongo_password = process.env['MONGO_PASSWORD'];
+const uri = `mongodb+srv://${mongo_username}:${mongo_password}@cluster0.yap72ns.mongodb.net/?retryWrites=true&w=majority`;
+const port = 8000;
 
-const main = document.querySelector("#section");
-const form = document.querySelector("#form");
-const search = document.querySelector("#query");
-
-returnMovie(APILINK);
-
-function returnMovie(url) {
-   fetch(url).then(res => res.json()).then(function (data) {
-      console.log(data.results)
-      data.results.forEach(element => {
-         const div_card = document.createElement("div");
-         div_card.setAttribute("class", "card");
-         const row = document.createElement("div");
-         row.setAttribute("class", "row");
-         const column = document.createElement("div");
-         column.setAttribute("class", "column");
-         const image = document.createElement("img");
-         image.setAttribute("class", "thumpnail");
-         const title = document.createElement("h3");
-
-         title.innerHTML = `${element.title}`;
-         image.src = IMG_PATH + element.poster_path;
-
-         div_card.appendChild(image);
-         div_card.appendChild(title);
-         column.appendChild(div_card);
-         row.appendChild(column);
-
-
-         main.appendChild(row);
-      });
-   });
-}
-
-form.addEventListener("submit", e => {
-   e.preventDefault();
-   main.innerHTML = "";
-
-   const searchItem = search.value;
-
-   if (searchItem) {
-      returnMovie(SEARCHAPI + searchItem);
-      searchItem = "";
-   }
-})
+MongoClient.connect(
+   uri,
+   {
+      maxPoolSize: 50,
+      wtimeoutMS: 2500,
+      useNewUrlParser: true
+   })
+   .catch(err => {
+      console.error(err.stack);
+      process.exit(1);
+   })
+   .then(async client => {
+      await ReviewsDAO.injectDB(client);
+      app.listen(port, () => {
+         console.log(`listening on port ${port}`)
+      })
+   })
